c8943976885a5c6385bb305c0d61a325d725d38f
MichaelBarz <mb@barz.de>
2015-06-17 16:48:49 +0200

    add missing files for undistortion wrapper

--- pySLAM.pyx
+++ pySLAM.pyx
@@ -1,11 +1,17 @@
 from slam_system cimport Matrix3f
 from slam_system cimport SlamSystem 
+from slam_system cimport Undistorter
 
-cdef class pySLAM:
+cdef class Slam_Context:
     cdef SlamSystem *thisptr
-    def __cinit__(self, int w, int h, enableSLAM=True):
-        cdef Matrix3f K =  Matrix3f()
-        self.thisptr = new SlamSystem(w, h, K, enableSLAM)
+    def __cinit__(self, int w, int h,float[::1] K, enableSLAM=True):
+        cdef Matrix3f _K
+        cdef float * K_d = _K.data()
+
+        for x in range(9):
+            K_d[x] = K[x]
+            print K_d[x]
+        self.thisptr = new SlamSystem(w, h, _K, enableSLAM)
         
     def __init__(self, int w, int h, enableSLAM=True):
         pass
@@ -13,12 +19,23 @@ cdef class pySLAM:
     def __dealloc__(self):
         del self.thisptr
         
-    def init(self, image, ts, id):
-        self.thisptr.randomInit(image, ts, id)
-    def track_frame(self, image, id, blockedUntilMapped, ts):
-        self.thisptr.trackFrame(image, id, blockedUntilMapped, ts)
+    def init(self, unsigned char[:,::1] image,int id, double ts):
+        self.thisptr.randomInit(&image[0,0], ts, id)
+        
+    def track_frame(self,unsigned char[:,::1] image, int id, double ts, bint blockedUntilMapped):
+        self.thisptr.trackFrame(&image[0,0], id, blockedUntilMapped, ts)
+        
     def finalize(self):
         self.thisptr.finalize()
-        
+
     #def set_output_wrapper(self, output_wrapper):
     #   self.thisptr.setVisualization(output_wrapper)
+
+cdef class Slam_Undistorter:
+    cdef Undistorter *thisptr
+    
+    def __cinit__(self):
+        pass
+    
+    def get_instance(self, camera_config_path):
+        self.thisptr = Undistorter.getUndistorterForFile(camera_config_path)
--- slam_system.pxd
+++ slam_system.pxd
@@ -2,25 +2,33 @@ from libcpp.string cimport string
 from io_wrapper cimport Output3DWrapper
 
 cdef extern from "<Eigen/Eigen>" namespace "Eigen":
-    ctypedef struct Matrix3f:
-        pass 
+    cdef cppclass Matrix3f:
+        Matrix3f() except + 
+        Matrix3f(int rows, int cols) except + 
+        float * data()
 
 cdef extern from "SlamSystem.h" namespace "lsd_slam":
-
-    
-    #ctypedef struct Output3DWrapper:
-    #    pass
     ctypedef struct Frame:
         pass
 
     cdef cppclass SlamSystem:
         SlamSystem(int, int, Matrix3f, bool) except +
         
-        void randomInit(unsigned char*, double, int)
-        void trackFrame(unsigned char*, unsigned int, bool, double)
+        void randomInit(unsigned char* image, double timeStamp, int id)
+        void trackFrame(unsigned char* image, unsigned int frameID, bint blockUntilMapped, double timestamp)
         
         void finalize()
         
         void setVisualization(Output3DWrapper*)
         
         Frame* getCurrentKeyframe()
+        
+cdef extern from "<util/Undistorter.h>" namespace "lsd_slam":
+    cdef cppclass Undistorter:
+        Undistorter() except +
+        
+        @staticmethod
+        Undistorter* getUndistorterForFile(const char* configFilename)
+        
+        void undistort(unsigned char*, unsigned char*)
+        
